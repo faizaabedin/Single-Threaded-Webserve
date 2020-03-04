1ffdae73014ce0232abe7ac9912dc842b795067d
rehanhajee <rehanhajee@gmail.com>
2020-03-04 14:42:16 -0500

    Update Assignment1.js

--- Assignment1.js
+++ Assignment1.js
@@ -8,6 +8,7 @@ var dir_path = "";
 var fileName = ""
 var currentInput = "";
 var previousInput = "";
+var fileTypes = [];
 
 let date  = new Date().toString();
 
@@ -19,6 +20,7 @@ net.createServer((socket) => {
 		if (err) throw err;
 		let content = data.split("\r")[0];
 		dir_path = content.split(" ")[1];
+		fileTypes = (data.split("\r")[1]).split(" ");
 	});
 	
 	socket.on("end", () => {
@@ -107,26 +109,33 @@ function getFile(socket) {
 
 function createFile(request, socket) {
 	let data = "";
-	try{
-		if (request[1].split(" ")[1] > 0) {
-			for (let i = 2; i < request.length - 3; i++) {
-				if (data != undefined) data += request[i] + "\n";
+	let extension = fileName.split(".")[1];
+	if (fileTypes.indexOf(extension) != -1) {
+		try {
+			if (request[1].split(" ")[1] > 0) {
+				for (let i = 2; i < request.length - 3; i++) {
+					if (data != undefined) data += request[i] + "\n";
+				}
+				
+				fs.appendFile(dir_path + fileName, data.substr(0,request[1].split(" ")[1]), function (err) {
+					if (err) throw err;
+					let response = "HTTP/1.0 201 Created\nContent-Length: " + data.length + "\nServer: pizza.scs.ryerson.ca\nConnection: close\nContent-Type: text/html; charset=utf-8\nDate: " + date.split(" (")[0] + "\nContent-Language: en-us\nLast-Modified: " + date.split(" (")[0];
+					socket.write(response + "\n\n\n" + fileName.substr(1) + " was successfully created!\n\n");
+					socket.pipe(socket);
+				});
+			} else {
+				socket.write("HTTP/1.0 400 Bad Request\nServer: pizza.scs.ryerson.ca\nConnection: close\nDate: " + date.split(" (")[0] + "\n\n");
 			}
-
-			fs.appendFile(dir_path + fileName, data, function (err) {
-				if (err) throw err;
-				let response = "HTTP/1.0 201 Created\nContent-Length: " + data.length + "\nServer: pizza.scs.ryerson.ca\nConnection: close\nContent-Type: text/html; charset=utf-8\nDate: " + date.split(" (")[0] + "\nContent-Language: en-us\nLast-Modified: " + date.split(" (")[0];
-				socket.write(response + "\n\n\n" + fileName.substr(1) + " was successfully created!\n\n");
-				socket.pipe(socket);
-			});
-		} else {
+			socket.pipe(socket);
+		} catch(e) {
 			socket.write("HTTP/1.0 400 Bad Request\nServer: pizza.scs.ryerson.ca\nConnection: close\nDate: " + date.split(" (")[0] + "\n\n");
+			socket.pipe(socket);
 		}
-		socket.pipe(socket);
-	} catch(e) {
+	} else {
 		socket.write("HTTP/1.0 400 Bad Request\nServer: pizza.scs.ryerson.ca\nConnection: close\nDate: " + date.split(" (")[0] + "\n\n");
 		socket.pipe(socket);
 	}
+	
 }
 
 function getHead(socket) {
@@ -138,7 +147,7 @@ function getHead(socket) {
 			
 			if (data.length != 0) {
 				let response = "HTTP/1.0 200 OK\nContent-Length: " + data.length + "\nServer: pizza.scs.ryerson.ca\nConnection: close\nContent-Type: text/html; charset=utf-8\nDate: " + date.split(" (")[0] + "\nContent-Language: en-us\nLast-Modified: " + date.split(" (")[0];
-				socket.write(response + "\n\n\n" + data + "\n\n");
+				socket.write(response + "\n\n");
 			} else {
 				socket.write("HTTP/1.0 403 No Read Permissions\nServer: pizza.scs.ryerson.ca\nConnection: close\nDate: " + date.split(" (")[0] + "\n\n");
 			}
